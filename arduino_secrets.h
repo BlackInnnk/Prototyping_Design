9c72d5f6bb48a177bec340b904f3b9c9c62407a3
BlackInnnk <151658530+BlackInnnk@users.noreply.github.com>
2025-10-29 04:30:57 +0000

    Reset credentials in arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,9 +1,5 @@
-// #define SECRET_SSID "CE-Hub-Student"
-// #define SECRET_PASS "casa-ce-gagarin-public-service"
-#define SECRET_SSID "Innnk_2.4GHz"
-#define SECRET_PASS "Bywwo94wo....."
-// #define SECRET_SSID "Black_Innnk"
-// #define SECRET_PASS "asdfghjkl..."
-#define SECRET_MQTTUSER "student"
-#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
+#define SECRET_SSID " "
+#define SECRET_PASS " "
+#define SECRET_MQTTUSER " "
+#define SECRET_MQTTPASS " "
 
